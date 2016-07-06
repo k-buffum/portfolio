8c5f0011055cad886da2913682cea63e18e83932
k-buffum <knbuffum@gmail.com>
2016-07-06 15:38:30 -0700

    Content added to projects page and about page

---
+++ js/app.js
@@ -0,0 +1,28 @@
+angular.module('Portfolio', ['ui.router'])
+
+.config([
+  '$stateProvider',
+  '$urlRouterProvider',
+  function($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/');
+
+    //define routes
+    $stateProvider
+    .state('projects', {
+      url: '/',
+      templateUrl: 'views/projects.html'
+    })
+
+    .state('about', {
+        url: '/about',
+        templateUrl: 'views/about.html'
+      }
+    )
+
+    .state('contact', {
+        url: '/contact',
+        templateUrl: 'views/contact.html'
+      }
+    );
+  }
+]);
